f85e5fe707dcf52ff7fcc844deb37e71ee1eb5c8
MaciejCzerniak243552 <243552@buas.nl>
2026-01-27 01:18:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -199,7 +199,7 @@ st.markdown(
         --header-offset: 40px;
         --footer-height: 80px;
         --input-height: 90px;
-        --sidebar-width: 18vw;
+        --sidebar-width: 15.5vw;
     }
     * {
         box-sizing: border-box;
